4bd035506ede604d37af98f7bc267b7843249eb2
Fabien M <f.mathe.pro@outlook.fr>
2025-11-10 16:19:11 +0100

    Try to fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
-export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
-});
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	plugins: [tailwindcss(), sveltekit()],
+	extensions: ['.svelte', '.md', '.svx'],
+	preprocess: [
+		vitePreprocess({}),
+		mdsvex({
+			extensions: ['.md', '.svx']
+		})
+	],
+};
+
+export default config;
